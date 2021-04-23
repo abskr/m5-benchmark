475b2b13f3fbb5a3c930fe5bc87ca10ccaf41b2b
ABskr <bskr26@gmx.de>
2021-04-23 14:52:13 +0200

    reviews changed

--- src/reviews/index.js
+++ src/reviews/index.js
@@ -19,11 +19,11 @@ const reviewsJson = join(dirname(fileURLToPath(import.meta.url)), '../data/jsonD
 router.get('/:imdbID', async (req, res, next) => {
   try {
     const reviews = await fs.readJson(reviewsJson)
-    const selectedReviews = reviews.filter(review => review.imdbID === req.params.imdbID)
-    if (selectedReviews.index === -1) {
-      res.send({msg: 'No comment found!'})
+    const reviewIndex = reviews.findIndex(review => review.imdbID === req.params.imdbID)
+    if (reviewIndex !== -1) {
+      res.status(200).send(reviews[reviewIndex])
     } else { 
-      res.status(200).send(reviews)
+      res.send({msg: 'No comment found!'})
     }
   } catch (error) {
     console.log(error)
@@ -42,24 +42,28 @@ router.post('/:imdbID', reviewsValidator, async(req, res, next) => {
       next(error)
     }
     const media = await fs.readJson(mediaJson)
-    const selectedMedium = media.filter(medium => medium.imdbID === req.params.imdbID)
-    if (!selectedMedium) {
+    const mediumIndex = media.findIndex(medium => medium.imdbID === req.params.imdbID)
+    //console.log(mediumIndex)
+    if (mediumIndex !== -1) {
+      const reviews = await fs.readJson(reviewsJson)
+      const newReview = {
+        ...req.body,
+        _id: uniqid(),
+        imdbID: req.params.imdbID,
+        createdAt: new Date()
+      }
+      reviews.push(newReview)
+      await fs.writeJson(reviewsJson, reviews)
+      res.status(201).send({
+        msg: "comment uploaded"
+      })
+    } else {
       const error = new Error({
         errMsg: "Medium not found!"
       })
       error.httpStatusCode = 404
       next(error)
     }
-    const reviews = await fs.readJson(reviewsJson)
-    const newReview = {
-      ...req.body,
-      _id: uniqid(),
-      imdbID: req.params.imdbID,
-      createdAt: new Date()
-    }
-    reviews.push(newReview)
-    await fs.writeJson(reviewsJson, reviews)
-    res.status(201).send(newReview)
   } catch (error) {
     console.log(error)
     next(error)
@@ -69,8 +73,8 @@ router.post('/:imdbID', reviewsValidator, async(req, res, next) => {
 router.put('/:imdbID/comment/:_id', async (req, res, next) => {
   try {
     const media = await fs.readJson(mediaJson)
-    const selectedMedium = media.find(medium => medium.imdbID === req.params.imdbID)
-    if (!selectedMedium) {
+    const mediumIndex = media.findIndex(medium => medium.imdbID === req.params.imdbID)
+    if (mediumIndex <= -1) {
       const error = new Error({
         errMsg: "Medium not found!"
       })
@@ -79,26 +83,28 @@ router.put('/:imdbID/comment/:_id', async (req, res, next) => {
     }
 
     const reviews = await fs.readJson(reviewsJson)
-    const selectedReview = reviews.find(review => review._id === req.params._id)
-    if (!selectedReview) {
-      const error = new Error({
+    const reviewIndex = reviews.findIndex(review => review.imdbID === req.params.imdbID)
+    if (reviewIndex <= -1) {
+       const error = new Error({
         errMsg: "Review not found!"
       })
       error.httpStatusCode = 404
       next(error)
+    } else {
+      const modReview = {
+        ...req.body,
+        imdbID: req.params.imdbID,
+        _id: req.params._id,
+        updatedAt: new Date()
+      }
+      const modReviews = reviews.filter(reviews => reviews._id !== req.params.id)
+
+      modReviews.push(modReview)
+      await fs.writeJson(reviewsJson, modReviews)
+      res.send(modReview)
     }
 
-    const modReview = {
-      ...req.body,
-      imdbID: req.params.imdbID,
-      _id: req.params._id,
-      updatedAt: new Date()
-    }
-    const modReviews = reviews.filter(reviews => reviews._id !== req.params.id)
-
-    modReviews.push(modReview)
-    await fs.writeJson(reviewsJson, modReviews)
-    res.send(modReview)
+    
   } catch (error) {
     console.lof(error)
     next(error)
@@ -109,7 +115,7 @@ router.delete('/:imdbID/comment/:_id', async (req, res, next) => {
   try {
     const media = await fs.readJson(mediaJson)
     const findMedia = media.find(medium => medium.imdbID === req.params.imdbID)
-    if (!findMedia) {
+    if (findMedia.length === 0) {
       const error = new Error({
         errMsg: "Medium not found!"
       })
@@ -119,7 +125,7 @@ router.delete('/:imdbID/comment/:_id', async (req, res, next) => {
 
     const reviews = await fs.readJson(reviewsJson)
     const findReview = reviews.find(review => review._id === req.params._id)
-    if (!findReview) {
+    if (findReview.length === 0) {
       const error = new Error({
         errMsg: "Review not found!"
       })
@@ -130,7 +136,8 @@ router.delete('/:imdbID/comment/:_id', async (req, res, next) => {
     await fs.writeJson(reviewsJson, modReviews)
     res.status(204)
   } catch (error) {
-
+    console.log(error)
+    next(error)
   }
 })
 
